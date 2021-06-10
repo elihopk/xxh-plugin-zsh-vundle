e5cdc6750d0a10815afc6d7e8ef1f2fd846ab967
Eli Hopkins <ewh3157@rit.edu>
2021-06-10 03:35:54 -0400

    Initial Commit

--- build.sh
+++ build.sh
@@ -16,27 +16,28 @@ done
 rm -rf $build_dir
 mkdir -p $build_dir
 
+cd $build_dir
+
 for f in pluginrc.zsh
 do
     cp $CDIR/$f $build_dir/
 done
 
-#portable_url='https://,,,/.tar.gz'
-#tarname=`basename $portable_url`
-#
-#cd $build_dir
-#
-#[ $QUIET ] && arg_q='-q' || arg_q=''
-#[ $QUIET ] && arg_s='-s' || arg_s=''
-#[ $QUIET ] && arg_progress='' || arg_progress='--show-progress'
-#
-#if [ -x "$(command -v wget)" ]; then
-#  wget $arg_q $arg_progress $portable_url -O $tarname
-#elif [ -x "$(command -v curl)" ]; then
-#  curl $arg_s -L $portable_url -o $tarname
-#else
-#  echo Install wget or curl
-#fi
-#
-#tar -xzf $tarname
-#rm $tarname
+vim_home=$build_dir/.vim
+
+rm -rf $vim_home
+mkdir -p $vim_home
+mkdir -p $vim_home/bundle
+
+cd $vim_home
+
+[ $QUIET ] && arg_q='-q' || arg_q=''
+[ $QUIET ] && arg_s='-s' || arg_s=''
+[ $QUIET ] && arg_progress='' || arg_progress='--show-progress'
+
+if [ -x "$(command -v git)" ]; then
+  git clone $arg_q --depth 1 https://github.com/VundleVim/Vundle.vim.git $vim_home/bundle/Vundle.vim 
+else
+  echo Install git
+  exit 1
+fi
--- pluginrc.zsh
+++ pluginrc.zsh
@@ -1,4 +1,53 @@
-# This script will be executed ON THE HOST when you connect to the host.
-# Put here your functions, environment variables, aliases and whatever you need.
+# XXH ZSH Plugin for Vundle
 
-CURR_DIR="$(cd "$(dirname "$0")" && pwd)"
+cd "$(dirname "$0")" 1>/dev/null
+CURR_DIR="$(pwd)"
+PLUGIN_NAME="xxh-plugin-zsh-vundle"
+
+if [[ -v vundles ]]
+then
+    if [[ $XXH_VERBOSE == '2' ]]
+    then
+        echo "$PLUGIN_NAME: Vundles is set = $vundles"
+    fi
+else
+    if [[ $XXH_VERBOSE == '2' ]]
+    then
+        echo "$PLUGIN_NAME: Vundles not set"
+        exit 1
+    fi
+fi
+
+newVundles=("${(@s/|/)vundles}")
+
+rm pluginvimrc.vim 2>/dev/null
+touch pluginvimrc.vim
+
+echo -e "set nocompatible\nfiletype off\nset rtp+=$CURR_DIR/.vim/bundle/Vundle.vim\ncall vundle#begin()\nPlugin 'VundleVim/Vundle.vim'" >> pluginvimrc.vim
+
+
+for i in "${newVundles[@]}"
+do
+    echo "Plugin $i" >> pluginvimrc.vim
+done
+
+echo -e "call vundle#end()\nfiletype plugin indent on" >> pluginvimrc.vim
+
+if [[ -v customrc ]]
+then
+    newCustomRC=("${(@s/|/)customrc}")
+
+    for i in "${newCustomRC[@]}"
+    do
+        echo $i >> pluginvimrc.vim
+    done
+fi
+
+if [[ -f pluginvimrc.vim ]]
+then
+    export VIMINIT="source $CURR_DIR/pluginvimrc.vim"
+else
+    echo "$PLUGIN_NAME: pluginvimrc.vim not found!"
+fi
+
+vim +PluginInstall +qall -e -s
